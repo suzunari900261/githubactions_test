d14347370433b0413174784b7bbfcd31975076f4
suzunari900261 <uz7184@gmail.com>
2026-02-01 17:21:07 +0900

    update

--- lib/github_actions-stack.ts
+++ lib/github_actions-stack.ts
@@ -1,16 +1,17 @@
-import * as cdk from 'aws-cdk-lib/core';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from 'aws-cdk-lib';
+import {Construct} from 'constructs';
+import * as s3 from 'aws-cdk-lib/aws-s3';
 
-export class GithubActionsStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+export class GithubActionsStack extends cdk.Stack{
+    constructor(scope: Construct, id: string, props?: cdk.StackProps){
+        super(scope, id, props);
 
-    // The code that defines your stack goes here
+new s3.Bucket(this, 'SampleBucket',{
+    versioned: true,
+    removalPolicy: cdk.RemovalPolicy.DESTROY,
+    autoDeleteObjects: true,
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'GithubActionsQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    });
   }
 }
+
